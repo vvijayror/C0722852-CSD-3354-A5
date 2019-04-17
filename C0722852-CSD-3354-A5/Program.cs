535ccfbcfc20425eb8d8a56be78df04a1c1dafa6
visualstudio <visualstudio@122-PC14>
2019-04-17 09:43:11 -0400

    commit

--- C0722852-CSD-3354-A5/Program.cs
+++ C0722852-CSD-3354-A5/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,45 @@ namespace C0722852_CSD_3354_A5
     {
         static void Main(string[] args)
         {
+            GenerateList a = new GenerateList();
+            a.Run(57);
+        }
+        class GenerateList
+        {
+            const int LEN = 100;
+
+            ArrayList ListQ;
+            public void Run(int AddUpToNumber)
+            {
+                Random r = new Random();
+
+                ListQ = new ArrayList();
+                for (int i = 0; i < LEN; i++)
+                {
+                    ListQ.Add(r.Next(100));
+                    this.findTwoNumbersThatAddUpto(AddUpToNumber);
+                    break;
+                }
+            }
+
+            public void findTwoNumbersThatAddUpto(int a)
+            {
+                Random r = new Random();
+                int n1 = 0;
+                int n2 = 0;
+                for (int i = 0; i < LEN; i++)
+                {
+                    int num1 = ListQ.Add(r.Next(100));
+                    int num2 = ListQ.Add(r.Next(100));
+                    if (num1 + num2 == a)
+                    {
+                        n1 = num1;
+                        n2 = num2;
+                        Console.WriteLine($"The Two Numbers are the Folowing {n1} and {n2}.");
+                    }
+                }
+            }
         }
     }
 }
+
